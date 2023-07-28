c2035af820fc367c0e6fcb8cf84b5f89a5bac81c
NguyenThiQue <quenguyenbt@gmail.com>
2023-07-28 08:45:29 +0700

    update

--- Kho/models/Kho.py
+++ Kho/models/Kho.py
@@ -4,10 +4,12 @@ class Kho(models.Model):
     _name = 'kho'
     _description = ''
     name = fields.Char(string="Tên")
+    soluongthucte = fields.Integer(string="Số lượng thư tế")
     soluong = fields.Integer(string="Số lượng")
     diachi = fields.Char(string="Địa chỉ")
     # cauhinhkho = fields.Char(string="Cấu hình kho hàng")
     category = fields.Many2one("danhmucsp", string="Loại sản phẩm")
+    donvi = fields.Char(string="Đơn vị")
 
     # @api.model
     # def update_stock_quantity(self, ctdonhang):
